0278e85780ac031dc247067cd1629e1a5884ba55
Mahmoud Saad <mahmoudmsaad7@gmail.com>
2018-11-12 13:03:36 +0200

    finishing up all compiling issues, now time for debugging

---
+++ src/data.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+
+
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base)
+{
+	int abs_data = abs(data);
+	int i = 0;
+
+	while(abs_data)
+	{
+		int reminder = abs_data % base;
+	
+		if(reminder >= 10)
+		{
+			*(ptr + i) = 65 + (reminder - 10) ;
+			i++;
+		}		
+		
+		else
+		{
+			*(ptr + i) = 48 + reminder;
+			i++;
+		}
+
+		abs_data = abs_data / base;
+	
+
+	}
+
+	
+	if( i == 0)
+	{
+		*(ptr + i) = 0;
+		i++;
+	}
+
+
+	if(data < 0 && base == 10) /*if data input is negative and desired base is decimal base*/
+	{
+		*(ptr+i) = '-';
+		i++;	
+	}
+
+	*(ptr + i) = '\0'; /*Null terminator at the end*/
+	
+	
+	uint8_t counter = 0;
+	
+	while ( *(ptr + counter) != '\0' )
+	{
+		counter++;
+	}
+
+	uint8_t length_Of_Converted_Data = counter + 1;
+
+	ptr = my_reverse(ptr, length_Of_Converted_Data);
+		
+	return length_Of_Converted_Data;
+	
+}
+
+
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base)
+{
+	int32_t num = 0;
+ 	int i = 0;
+ 
+	// run till we have reached end of the string or
+	// current character is non-numeric
+	while (digits && (digits >= '0' && digits <= '9'))
+	{
+		num = num * 10 + (digits - '0');
+		i++;
+	}
+ 
+	return num;
+}
